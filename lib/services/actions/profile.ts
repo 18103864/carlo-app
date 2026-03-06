8e426f4ae1e24f227e4b9143d7628cdc89b80b39
18103864 <valorantalpha12@gmail.com>
2026-03-06 18:32:38 +0800

    feat: get profile details function added

--- lib/services/actions/profile.ts
+++ lib/services/actions/profile.ts
@@ -28,3 +28,33 @@ export async function setupProfile(name: string) {
 
     return { error: false, data }
 }
+
+export async function getProfile(id: string) {
+    const user = await getCurrentUser()
+
+    if(!user) {
+        return { error: true, message: 'User is not authenticated'}
+    }
+
+    if(!id.trim()){
+        return { error: true, message: 'Id cannot be empty'}
+    }
+
+    const supabase = await createClient()
+
+    const { data, error } = await supabase
+        .from('user_profile')
+        .select('*')
+        .eq('id', id)
+        .single()
+    
+    if(error){
+        return {error: true, message: 'Failed to get profile details'}
+    }
+
+    return {error: false, data}
+}
+
+export async function updateProfile(){
+    
+}
